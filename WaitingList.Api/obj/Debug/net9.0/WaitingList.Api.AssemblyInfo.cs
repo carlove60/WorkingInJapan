a0c5205bbd25ef23410ed16a4ccd1fbd76df8dbb
carlove <carlove60@hotmail.com>
2025-06-05 16:35:24 +0200

    errors added

--- WaitingList.Api/obj/Debug/net9.0/WaitingList.Api.AssemblyInfo.cs
+++ WaitingList.Api/obj/Debug/net9.0/WaitingList.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaitingList.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d3eebac07786e2963d45dae28a685eb8f4b5bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55a58a4a41a342f009c8c4d5d7fd743e3ea13cdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaitingList.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaitingList.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
